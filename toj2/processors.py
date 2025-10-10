bb33b15e0c202d700fca5fd04606b98d00b9e4a1
akio-1978 <akio.ogawa01@gmail.com>
2025-10-11 01:57:42 +0900

    Jinja2をバイパスして直接任意のpython関数へ出力するための仕組み

--- toj2/processors.py
+++ toj2/processors.py
@@ -3,18 +3,30 @@ from jinja2 import Environment, FileSystemLoader
 from .context import AppContext
 from .jinja2_custom_filter import sequential_group_by
 from .excel.excel_custom_filter import excel_time
-from .utils import get_stream
 
 class Processor:
     def __init__(self, context:AppContext) -> None:
         self.context = context
         self._setup()
+        self.output_func = None
+
+    # 読み取り結果を渡すオブジェクトを指定する
+    # メソッドの引数は(load_object:dict, context:AppContext)であること
+    def set_output_func(self, output_func: callable):
+        self.output_func = output_func
 
     def _setup (self):
         pass
 
-    def execute(self, loaded_object):
+    def execute(self, load_object:dict):
+        if (self.output_func is None):
+            self._execute(load_object)
+        else:
+            self.output_func(load_object, self.context)
+
+    def _execute(self, load_object):
         pass
+
 
 class Jinja2Processor(Processor):
     def __init__(self, context:AppContext) -> None:
@@ -32,8 +44,9 @@ class Jinja2Processor(Processor):
         """追加のフィルタを設定する"""
         environment.filters['sequential_group_by'] = sequential_group_by
         environment.filters['excel_time'] = excel_time
-        
-    def execute(self, loaded_object):
+
+    def _execute(self, load_object:dict):
         with open(self.context.out ,mode='wb') as dest:
-            loaded_object['params'] = self.context.parameters
-            dest.write(self.template.render(loaded_object).encode(self.context.output_encoding))
+            load_object['params'] = self.context.parameters
+            dest.write(self.template.render(load_object).encode(self.context.output_encoding))
+
