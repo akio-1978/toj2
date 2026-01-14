dbdd7bbaf9657d03c1acdad1ccd46b9fd4bbe199
akio-1978 <akio.ogawa01@gmail.com>
2026-01-15 04:54:00 +0900

    型ヒント

--- toj2/runner.py
+++ toj2/runner.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 import json
 import toj2
+from command import Command
 from .csv.csv_command import CsvCommand
 from .excel.excel_command import ExcelCommand
 from .json.json_command import JsonCommand
@@ -12,7 +13,7 @@ from .context import AppContext
 class Runner():
     """コマンドラインから呼び出される処理の起点となるクラス"""
 
-    def __init__(self, *, args:list):
+    def __init__(self, *, args:list) -> None:
         self.args = args
         self.PARSER_CMD = {
             'csv' : CsvCommand,
@@ -21,7 +22,7 @@ class Runner():
             'yaml' : YamlCommand
         }
 
-    def execute(self):
+    def execute(self) -> None:
         """プログラム実行"""
         # ファイル形式に対応した実際のコマンドを取得する
         # contextには設定ファイルの中身が書き込まれている
@@ -32,7 +33,7 @@ class Runner():
         # ※contextはImmutableではない
         command.execute(args=self.args, context=context)
 
-    def build_cmd(self, *, args:list):
+    def build_cmd(self, *, args:list) -> Command:
         """引数を一部だけパースしてcontextとcommandを取得する"""
         # サブコマンドと設定ファイルだけを取得する、サブコマンドが対象外の場合ここで終了する
         ctx_parser = CustomHelpParser(prog=toj2.PROG_NAME, add_help=False)
@@ -47,7 +48,7 @@ class Runner():
             self.load_config_file(context=context, filename=parsed.config_file)
         return self.get_cmd_instance(cmd_type=parsed.cmd), context
 
-    def load_config_file(self,*, context, filename):
+    def load_config_file(self,*, context: AppContext, filename: str) -> AppContext:
         """ 設定ファイルの内容をcontextにsetattrする"""
         with open(filename) as src:
             config = json.load(src)
