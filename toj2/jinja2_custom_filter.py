9a43ac9265805d72051f34cf7fa75e544ca1121e
akio-1978 <akio.ogawa01@gmail.com>
2026-03-04 10:46:24 +0900

    ステージ忘れ

--- toj2/jinja2_custom_filter.py
+++ toj2/jinja2_custom_filter.py
@@ -1,9 +1,11 @@
 import itertools
+import datetime
 
-def stable_group_by(iterable, key):
+def stable_groupby(iterable, key):
     
     groups = []
     for group_key, group in itertools.groupby(iterable, lambda item: item.get(key, 'None')) :
         groups.append((group_key, list(group)))
 
     return groups
+
